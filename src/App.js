7a03c1a9dee8be0d53a8138f46562a0edfd734d0
raizada07 <topbugbounty@gmail.com>
2025-08-04 08:52:37 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -172,7 +172,7 @@ function App() {
       )}
 {showTutorForm && (
  <section className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50 px-4 overflow-y-auto">
-<div className="bg-white rounded-xl shadow-xl w-full max-w-xl p-6 my-10">
+<div className={`bg-white rounded-xl shadow-xl w-full max-w-xl p-6 ${window.innerWidth < 768 ? "mt-0" : "mt-auto mb-10"}`}>
 
     <h2 className="text-3xl font-bold text-center text-gray-800 mb-6">Apply as a Tutor</h2>
     
