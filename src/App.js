65e58db2bc78df8e1f0d5198e073baf80ad09092
raizada07 <topbugbounty@gmail.com>
2025-08-04 16:50:13 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -27,11 +27,11 @@ function App() {
   return (
     <div className="font-sans overflow-x-hidden">
       {/* Navbar */}
- <nav className="bg-white shadow-md p-4 flex justify-between items-center">
+<nav className="bg-white shadow-md p-4 flex justify-between items-center">
   <div className="flex items-center gap-2 mx-auto md:mx-0">
     <img src={logo} alt="GyaanSaathi Logo" className="w-16 sm:w-20 md:w-24 h-auto" />
     <h1 className="text-base sm:text-lg md:text-2xl font-bold text-blue-600 text-center">
-      Welcome to GyaanSaathi Home Tutor
+      Welcome To GyaanSaathi Home Tutor 
     </h1>
   </div>
 </nav>
