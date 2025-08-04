9f5cab60f5528c86bdecc39ae801263ecbf6ba59
raizada07 <topbugbounty@gmail.com>
2025-08-04 17:35:37 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -86,7 +86,7 @@ function App() {
       {/* Form */}
       {showForm && (
         <section className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50 px-4">
-          <div className={`bg-white rounded-xl shadow-xl w-full max-w-xl p-6  "mt-0" : "mt-auto mb-10"}`}>
+          <div className={`bg-white rounded-xl shadow-xl w-full max-w-xl p-6  "mt-0" : "mt-auto mb-10"'}>
             <h2 className="text-3xl font-bold text-center text-gray-800 mb-4">Book a Free Demo Class</h2>
 
             {formSubmitted && (
@@ -169,7 +169,7 @@ function App() {
       )}
 {showTutorForm && (
  <section className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50 px-4">
-<div className={`bg-white rounded-xl shadow-xl w-full max-w-xl p-6 "mt-0" : "mt-auto mb-10"}`}>
+<div className={`bg-white rounded-xl shadow-xl w-full max-w-xl p-6 "mt-0" : "mt-auto mb-10"`}>
 
     <h2 className="text-3xl font-bold text-center text-gray-800 mb-6">Apply as a Tutor</h2>
     
