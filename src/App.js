98861a5c4552a1d9b24579fdbcc6628f58ea9262
raizada07 <topbugbounty@gmail.com>
2025-08-05 06:55:52 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -240,7 +240,9 @@ function App() {
   </section>
 )}
       {/* Footer */}
-      <Footer />
+      <footer className="bg-blue-600 text-white text-center py-4 mt-12">
+        <p>&copy; 2025 GyaanSaathi. All rights reserved.</p>
+      </footer>
         
     </div>
   );
